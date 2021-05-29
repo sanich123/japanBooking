f3c24b706538880ea5efe73f69a2b3d37e5e9c00
sanich123 <aovoronin.piano@gmail.com>
2021-05-29 18:52:22 +0300

    Yobaniy es lint, doesn't work properly

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-let getRandomNumber = function (min, max) {
+const getRandomNumber = function (min, max) {
   if (min >= 0 && max > 0) {
-   let randomNumber = min - 0.5 + Math.random() * (max - min + 1);
+   const randomNumber = min - 0.5 + Math.random() * (max - min + 1);
    return(Math.round(randomNumber));
   }
-  return(console.log("Function doesn't work"));
-}
+  return('Function doesnt work');
+};
 
 getRandomNumber(1.5,2);
 
@@ -13,13 +13,13 @@ getRandomNumber(1.5,2);
 // либо использовать Math.round, тогда диапазон будет от min - 0.5 до max - min, либо Math.floor, соответственно диапазон будет от min до max + 1,
 // с отрицательными числами не понял, что делать в самой задаче
 
-let getRandomNumberFixed = function(min, max, decimalPlaces) {
+const getRandomNumberFixed = function(min, max, decimalPlaces) {
   if (min >= 0 && max > 0 && decimalPlaces >=0) {
-   let randomNumber = min - 0.5 + Math.random() * (max - min + 1);
-   return(+randomNumber.toFixed(decimalPlaces));
+    const randomNumber = min - 0.5 + Math.random() * (max - min + 1);
+    return(+randomNumber.toFixed(decimalPlaces));
   }
-  return(console.log("Function doesn't work"));
-}
+  return('Function doesnt work');
+};
 
 getRandomNumberFixed(0, 23, 9);
 
