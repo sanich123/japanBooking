2dde9b7a920e37d2b8ee6926ccd6fd51045d4922
sanich123 <aovoronin.piano@gmail.com>
2021-06-07 20:45:34 +0300

    Исправленные функции по генерации случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,28 +1,114 @@
-const getRandomNumber = function (min, max) {
-  if (min >= 0 && max > 0) {
-    const randomNumber = min - 0.5 + Math.random() * (max - min + 1);
-    return(Math.round(randomNumber));
-  }
-  return('Function doesnt work');
+const getRandomPositiveInteger = function  (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-getRandomNumber(1.5,2);
+//console.log(getRandomPositiveInteger(-57.989879, 58.989979));
 
-//Решение разбирал с https://learn.javascript.ru/task/random-min-max
-//Math.random() генерит случайное число из диапазона от 0 до 1. Дальше вопрос чисто к округлению,
-// либо использовать Math.round, тогда диапазон будет от min - 0.5 до max - min, либо Math.floor, соответственно диапазон будет от min до max + 1,
-// с отрицательными числами не понял, что делать в самой задаче
-
-const getRandomNumberFixed = function(min, max, decimalPlaces) {
-  if (min >= 0 && max > 0 && decimalPlaces >=0) {
-    const randomNumber = min - 0.5 + Math.random() * (max - min + 1);
-    return(+randomNumber.toFixed(decimalPlaces));
-  }
-  return('Function doesnt work');
+const getRandomPositiveDecimal = function (a, b, decimalPlaces) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return result.toFixed(decimalPlaces);
 };
 
-getRandomNumberFixed(0, 23, 9);
+//console.log(getRandomPositiveDecimal(-43.89798, -48.95678, 5));
 
-//Тоже самое, но без округления и немного пришлось погуглить метод toFixed
-// С отрицательными значениями не понял, что делать надо
-// и большие значения после запятой почему-то не работают, не знаю, типа 25, 30 и так далее.
+
+const TYPE_OF_ROOMS = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECK_IN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECK_OUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const numberOfCopies = 10;
+
+const getRandomArrayMember = (array) => {
+  const randomMember = array[getRandomPositiveInteger(1, 6)];
+  return randomMember;
+};
+console.log(getRandomArrayMember(FEATURES));
+
+
+const getRandomArrayLength = (array) => {
+  const newLength = _.random(0, array.length);
+  return array.slice(newLength);
+};
+
+console.log(getRandomArrayLength(FEATURES));
+
+
+const createAuthor = () => ({
+  avatar: `img/avatars/user0${  _.random(1, 10)  }.png`,
+});
+
+const createOffer = () => ({
+  title: 'Предлагаем снять уютную двушку на окраине Токио',
+  address: {
+    lat: _.random(35.65000, 35.70000).toFixed(5),
+    lng: _.random(139.70000, 139.80000).toFixed(5),
+  },
+  price: parseInt(_.random(0, Infinity)),
+  type: TYPE_OF_ROOMS[_.random(0, TYPE_OF_ROOMS.length - 1)],
+  rooms: parseInt(_.random(0, Infinity)),
+  guests: parseInt(_.random(0, Infinity)),
+  checkin: CHECK_IN[_.random(0, CHECK_IN.length - 1)],
+  checkout: CHECK_OUT[_.random(0, CHECK_OUT.length - 1)],
+  features: getRandomArrayLength(FEATURES),
+  description: 'Какое-то описание',
+  photos: getRandomArrayLength(PHOTOS),
+});
+
+//console.log(createOffer());
+
+const createLocation = () => ({
+  lat: _.random(35.65000, 35.70000).toFixed(5),
+  lng: _.random(139.70000, 139.80000).toFixed(5),
+});
+
+//console.log(createLocation());
+
+const getCreateCard = () => (
+  {
+    author: createAuthor(),
+    offer: createOffer(),
+    location: createLocation(),
+  }
+);
+
+console.log(getCreateCard());
+
+const offers = new Array(numberOfCopies).fill('').map(() => getCreateCard());
+
+console.log(offers);
