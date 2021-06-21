e730c7d7196232cf433c33dcd8e11d98c6ba2985
sanich123 <aovoronin.piano@gmail.com>
2021-06-21 12:52:05 +0300

    Added disable/enable function, function is in util.js file

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,3 +1,5 @@
+import { inputPrice } from './validation.js';
+
 //Соответствие комнат и гостей
 const roomNumber = document.querySelector('#room_number');
 const roomCapacity = document.querySelector('#capacity');
@@ -5,71 +7,9 @@ const roomCapacity = document.querySelector('#capacity');
 //Соответствие типа жилья и цены
 const typeOfHouse = document.querySelector('#type');
 
-//Валидация полей ввода title and price
-const inputTitle = document.querySelector('#title');
-const inputPrice = document.querySelector('#price');
-const inputAddress = document.querySelector('#address');
-// const MIN_TITLE_LENGTH = 30;
-// const MAX_TITLE_LENGTH = 100;
-// const MAX_PRICE = 1000000;
-
-//Валидация полей ввода title and price
-inputPrice.addEventListener('invalid', () => {
-  if (inputPrice.validity.valueMissing) {
-    inputPrice.setCustomValidity('Обязательное поле');
-  } else if (inputPrice.validity.rangeOverflow) {
-    inputPrice.setCustomValidity('Максимальное значение — 1 000 000');
-  } else if (inputPrice.validity.rangeUnderflow) {
-    inputPrice.setCustomValidity('Введенное значение меньше минимального');
-  } else if (inputPrice.validity.typeMismatch) {
-    inputPrice.setCustomValidity('Числовое поле');
-  } else {
-    inputPrice.setCustomValidity('');
-  }
-});
-
-inputTitle.addEventListener('invalid', () => {
-  if (inputTitle.validity.tooShort) {
-    inputTitle.setCustomValidity('Минимальная длина — 30 символов');
-  } else if (inputTitle.validity.tooLong) {
-    inputTitle.setCustomValidity('Максимальная длина — 100 символов');
-  } else if (inputTitle.validity.valueMissing) {
-    inputTitle.setCustomValidity('Обязательное текстовое поле');
-  } else {
-    inputTitle.setCustomValidity('');
-  }
-});
-
-inputAddress.addEventListener('invalid', () => {
-  if (inputAddress.validity.valueMissing) {
-    inputAddress.setCustomValidity('Обязательное поле');
-  } else {
-    inputAddress.setCustomValidity('');
-  }
-});
-
-// inputPrice.addEventListener('input', () => {
-//   const priceLength = inputPrice.value.length;
-//   if (priceLength > MAX_PRICE) {
-//     inputPrice.setCustomValidity(`Удалите лишние ${priceLength - MAX_PRICE} символы`);
-//   } else {
-//     inputPrice.setCustomValidity('');
-//   }
-//   inputPrice.reportValidity();
-// });
-
-// inputTitle.addEventListener('input', () => {
-//   const valueLength = inputTitle.value.length;
-//   if (valueLength < MIN_TITLE_LENGTH) {
-//     inputTitle.setCustomValidity(`Надо ввести еще ${MIN_TITLE_LENGTH - valueLength} символов`);
-//   } else if (valueLength > MAX_TITLE_LENGTH) {
-//     inputTitle.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} символы`);
-//   } else {
-//     inputTitle.setCustomValidity('');
-//   }
-//   inputTitle.reportValidity();
-// });
-
+//Синхронизация времени заезда - уезда
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
 
 //Соответствие типа жилья и цены
 typeOfHouse.addEventListener('change', () => {
@@ -101,17 +41,17 @@ roomNumber.addEventListener('change', () => {
   if (roomNumber[0].selected) {
     roomCapacity[0].disabled = true;
     roomCapacity[1].disabled = true;
-    roomCapacity[2].disabled = false;
+    roomCapacity[2].selected = true;
     roomCapacity[3].disabled = true;
   }
   if (roomNumber[1].selected) {
     roomCapacity[0].disabled = true;
-    roomCapacity[1].disabled = false;
+    roomCapacity[1].selected = true;
     roomCapacity[2].disabled = false;
     roomCapacity[3].disabled = true;
   }
   if (roomNumber[2].selected) {
-    roomCapacity[0].disabled = false;
+    roomCapacity[0].selected = true;
     roomCapacity[1].disabled = false;
     roomCapacity[2].disabled = false;
     roomCapacity[3].disabled = true;
@@ -120,12 +60,9 @@ roomNumber.addEventListener('change', () => {
     roomCapacity[0].disabled = true;
     roomCapacity[1].disabled = true;
     roomCapacity[2].disabled = true;
-    roomCapacity[3].disabled = false;
+    roomCapacity[3].selected = true;
   }
 });
-//Синхронизация времени заезда - уезда
-const timeIn = document.querySelector('#timein');
-const timeOut = document.querySelector('#timeout');
 
 timeIn.addEventListener('change', () => {
   if (timeIn[0].selected) {
