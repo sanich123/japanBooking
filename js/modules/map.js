62ee012d5c8e5e7ecc3bd43b04e255d91513e5c8
sanich123 <aovoronin.piano@gmail.com>
2021-06-25 14:21:37 +0300

    Changed longitude and lattitude to the value of Imperators palace

--- js/modules/map.js
+++ js/modules/map.js
@@ -15,8 +15,7 @@ const map = L
   .on('load', () => {
     enableFunction();
   })
-  .setView({lat: 35.68950,
-    lng: 139.69171}, 16);
+  .setView({lat: 35.68491, lng: 139.75159}, 14);
 //Добавляем слой с изображениями
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -34,14 +33,14 @@ const bigCustomIcon = L.icon({
 
 //Создаем большую метку
 const bigMarker = L.marker(
-  { lat: 35.68950, lng: 139.69171 },
+  { lat: 35.68491, lng: 139.75159},
   { draggable:true, icon: bigCustomIcon },
 ).addTo(map);
 
 //Обработчик перемещения большой метки
 bigMarker.on('moveend', (evt) => {
   const latLng = evt.target.getLatLng();
-  inputAddress.value = `lat: ${latLng.lat.toFixed(5)}, lng: ${  latLng.lng.toFixed(5)}`;
+  inputAddress.value = `${latLng.lat.toFixed(5)}, ${  latLng.lng.toFixed(5)}`;
 });
 
 // bigMarker.remove();
@@ -51,6 +50,56 @@ const customIcon = L.icon({
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
+// const createCustomPopup = () => {
+//   newOffers.forEach((newOffer) => {
+//     const realOffer = popupArticle.cloneNode(true);
+//     realOffer.querySelector('.popup__title').textContent = newOffer.offer.title;
+//     realOffer.querySelector('.popup__text--address').textContent = `${newOffer.offer.address.lat   },${  newOffer.offer.address.lng}`;
+//     realOffer.querySelector('.popup__text--price').textContent = `${newOffer.offer.price  } ₽/ночь`;
+//     realOffer.querySelector('.popup__type').textContent = newOffer.offer.type;
+//     switch (newOffer.offer.type) {
+//       case 'flat' : realOffer.querySelector('.popup__type').textContent = 'Квартира'; break;
+//       case 'bungalow' : realOffer.querySelector('.popup__type').textContent = 'Бунгало'; break;
+//       case 'house' : realOffer.querySelector('.popup__type').textContent = 'Дом'; break;
+//       case 'palace' : realOffer.querySelector('.popup__type').textContent = 'Дворец'; break;
+//       case 'hotel' : realOffer.querySelector('.popup__type').textContent = 'Отель'; break;
+//     }
+//     realOffer.querySelector('.popup__text--capacity').textContent = `${newOffer.offer.rooms}   комнаты для ${newOffer.offer.guests} гостей`;
+//     // eslint-disable-next-line no-useless-concat
+//     realOffer.querySelector('.popup__text--time').textContent = `Заезд после ${  newOffer.offer.checkin  },` +  ` выезд до ${  newOffer.offer.checkout}`;
+//     const features = realOffer.querySelector('.popup__features');
+//     features.innerHTML = '';
+
+//     FEATURES.forEach((currentValue) => {
+//       const feature = document.createElement('li');
+//       feature.classList.add('popup__feature');
+//       feature.classList.add(`popup__feature--${currentValue}`);
+//       features.appendChild(feature);
+//     });
+
+//     realOffer.querySelector('.popup__description').textContent = newOffer.offer.description;
+//     const photos = realOffer.querySelector('.popup__photos');
+//     photos.innerHTML = '';
+
+//     PHOTOS.forEach((currentValue) => {
+//       const photo = document.createElement('img');
+//       photo.classList.add('popup__photo');
+//       photo.src = currentValue;
+//       photo.width = 45;
+//       photo.height = 40;
+//       photo.alt = 'Фотография жилья';
+//       photos.appendChild(photo);
+//     });
+//     realOffer.querySelector('.popup__avatar').src = newOffer.author.avatar;
+//     for (let index = 0; index < popupArticle.length; index++) {
+//       if (popupArticle.children[index].textContent === '')  {
+//         popupArticle.children[index].style.display = 'none';
+//       }
+//     }
+//     similarOfferFragment.appendChild(realOffer);
+//   });
+// };
+// createCustomPopup(newOffer);
 
 newOffers.forEach((newOffer) => {
   const realOffer = popupArticle.cloneNode(true);
@@ -97,7 +146,6 @@ newOffers.forEach((newOffer) => {
       popupArticle.children[index].style.display = 'none';
     }
   }
-
   const marker = L.marker(
     {
       lat: newOffer.location.lat,
@@ -108,14 +156,17 @@ newOffers.forEach((newOffer) => {
     },
   );
   marker.addTo(map)
-    .bindPopup(similarOfferFragment.appendChild(realOffer));
+    .bindPopup(similarOfferFragment.appendChild(realOffer),
+      {
+        keepInView: true,
+      });
 });
 
 resetButton.addEventListener('click', () => {
   bigMarker.setLatLng({
-    lat: 35.68950, lng: 139.69171,
+    lat: 35.68491, lng: 139.75159,
   });
   map.setView({
-    lat: 35.68950, lng: 139.69171,
-  }, 16);
+    lat: 35.68491, lng: 139.75159,
+  }, 14);
 });
